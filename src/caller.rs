5cecf59e3d3678183bced214bcac9594e1df05c0
nguiard <nguiard@student.42.fr>
2023-03-09 15:32:40 +0100

    Nils came and did everything. Nice

--- src/caller.rs
+++ src/caller.rs
@@ -1,32 +1,100 @@
-#[derive(Clone, Default)]
+use std::{rc::Rc, cell::RefCell, collections::HashSet};
+
+
+#[derive(Clone, Debug, Default)]
 pub struct Caller {
 	pub name: String,
-	pub callees_struct: Vec<Callee>,
-	pub callees_name: Vec<String>,
+	pub callees: Vec<String>,
 }
 
-#[derive(Clone)]
-pub struct Callee {
-	function: Caller,
-	address_of_call: String,
+pub struct Node {
+	caller: Caller,
+	callees: RefCell<Vec<NodeType>>,
 }
 
-impl Caller {
-	pub fn clear(&mut self) {
-		self.name = String::new();
-		self.callees_name = vec![];
-		self.callees_struct = vec![];
-	}
+enum NodeType {
+	Local(Rc<Node>),
+	Extern(String),
+}
 
-	pub fn new() -> Self {
-		Caller {
-			name: String::new(),
-			callees_struct: vec![],
-			callees_name: vec![],
+
+impl Node {
+	pub fn create_graph(
+		it: impl Iterator<Item = Caller>,
+	) -> Vec<Rc<Self>> {
+		let a = Rc::new(String::from("test"));
+
+		let mut nodes: Vec<Rc<Self>> =
+			it
+			.map(|caller| {
+				Rc::new(Node {
+					caller,
+					callees: RefCell::new(Vec::new()),
+				})
+			})
+			.collect();
+
+		for caller in &nodes {
+			*caller.callees.borrow_mut() =
+				caller.caller.callees
+				.iter()
+				.map(|name| {
+					match nodes.iter().find(|n| n.caller.name == *name) {
+						Some(local) => NodeType::Local(local.clone()),
+						None => NodeType::Extern(name.clone()),
+					}
+				})
+				.collect()
 		}
+
+		nodes
 	}
 
-	pub fn is_empty(&self) -> bool {
-		self.name.is_empty()
+	pub fn visit<F>(&self, mut f: F)
+	where
+		F: FnMut(Option<&String>,  usize),
+	{
+		fn visit_inner<F>(this: &Node, visited: &mut HashSet<usize>, rec_no: usize, f: &mut F)
+		where
+			F: FnMut(Option<&String>, usize)
+		{
+			let addr = this as *const Node as usize;
+
+			if !visited.insert(addr) {
+				f(None, rec_no);
+				return;
+			}
+
+			f(Some(&this.caller.name), rec_no);
+
+			for callee in this.callees.borrow().iter() {
+				match callee {
+					NodeType::Extern(name) => f(Some(name), rec_no + 1),
+					NodeType::Local(callee) => visit_inner(callee, visited, rec_no + 1, f),
+				}
+			}
+
+			visited.remove(&addr);
+		}
+
+		let mut visited: HashSet<usize> = HashSet::new();
+		visit_inner(self, &mut visited, 0, &mut f);
+	}
+
+	pub fn display(&self) {
+		self.visit(|node, rec_no| {
+			for _ in 0..rec_no {
+				print!("\t");
+			}
+
+			match node {
+				Some(n) => println!("{n}"),
+				None => println!("..."),
+			}
+		});
+	}
+
+	pub fn name(&self) -> &str {
+		&self.caller.name
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use std::{env, collections::HashMap};
 mod caller;
 use caller::Caller;
+use caller::Node;
 use std::io::stdin;
 use std::io::Read;
 
+fn is_a_tty(fd: i32) -> bool {
+	extern "C" {
+		fn isatty(fd: std::ffi::c_int) -> std::ffi::c_int;
+	}
+
+	// SAFETY:
+	//  This function is always safe to call.
+	unsafe {isatty(fd) != 0 }
+}
 
 fn main() {
     let _argv: Vec<String> = env::args().collect();
@@ -16,12 +26,27 @@ fn main() {
 			Err(e) => panic!("Error : {:?}", e)
 		}
 
+			println!("stdin: {}", is_a_tty(0));
+			println!("stdout: {}", is_a_tty(1));
+
 	// }
     // } else {
     //     read_file();
     // }
 	// file_read = filtering(file_read);
-	let map = parsing(&file_read);
+	let callers = CallerList::new(file_read.lines());
+	let nodes = Node::create_graph(callers);
+
+	println!("FUNCTIONS:");
+	for node in &nodes {
+		println!("{}", node.name());
+	}
+	
+	println!();
+	println!();
+
+	nodes.iter().find(|node| node.name() == "main").unwrap().display();
+
 	// map = assign_callees(map);
 }
 
@@ -77,78 +102,74 @@ fn filtering(mut whole_file: Vec<String>) -> Vec<String> {
 	return whole_file;
 }
 
-fn parsing(data: &str) -> HashMap<String, Caller> {
-	let mut functions: HashMap<String, Caller> = HashMap::new();
-	let mut current_caller: Caller = Caller::new();
-
-	let mut in_text_section =  false;
-
-	for mut s in data.lines() {
-		s = s.trim();
-
-		if s.is_empty() {
-			continue;
-		}
-		
-		if !in_text_section {
-			if s.starts_with("Disassembly of section .text:") {
-				in_text_section = true;
-			}
-			
-			continue;
-		}
-
-		if s.starts_with("Disassembly of section") {
-			break;
-		}
-
-		if s.as_bytes().first() != Some(&b' ') {
-			let it = s.split_ascii_whitespace();
-			let name = it.last().unwrap();
-
-			if name.len() > 3 {
-				if !current_caller.is_empty() {
-					functions.insert(current_caller.name.clone(), current_caller);
-					current_caller = Caller::new();
-				}
-				current_caller.name = String::from(&name[1..name.len() - 2]);
-			}
-			else {
-				panic!("Parsing error: Name of function too short.");
-			}
-		} else {
-			let mut iter = s.split('\t');
-
-			if let Some(op) = iter.nth(2) {
-				if op.starts_with("call") {
-					current_caller.callees_name.push(parse_call_function(op));
-				}
-			}
-		}
-	}
-
-	if !current_caller.is_empty() {
-		functions.insert(current_caller.name.clone(), current_caller);
-	}
-
-	for (key, value) in &functions {
-		let truc = value.callees_name.clone();
-		for a in truc {
-			println!("{}: {}", key, a);
-		}
-	}
-
-	return functions;
+struct CallerList<I> {
+	lines: I,
 }
 
-fn parse_call_function(s: &str) -> String {
+impl<'a, I> CallerList<I>
+where
+	I: Iterator<Item = &'a str>,
+{
+	pub fn new(mut lines: I) -> Self {
+		for line in &mut lines {
+			if line.starts_with("Disassembly of section .text:") {
+				break;
+			}
+		}
+
+		Self { lines }
+	}
+}
+
+impl<'a, I> Iterator for CallerList<I>
+where
+	I: Iterator<Item = &'a str>,
+{
+	type Item = Caller;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		// Find the name.
+		let almost_name = self
+			.lines
+			.find(|line| !line.is_empty())?
+			.split_ascii_whitespace()
+			.last()
+			.unwrap();
+
+		let name = almost_name[1..almost_name.len() - 2].to_string();
+
+		// Find the callees.
+		let callees: Vec<String> =
+			(&mut self.lines)
+			.take_while(|line| !line.is_empty())
+			.filter_map(|line| {
+				line.split('\t')
+					.nth(2)
+					.and_then(parse_call_function)
+			})
+			.map(str::to_string)
+			.collect();
+
+		Some(Caller {
+			name,
+			callees
+		})
+	}
+}
+
+fn parse_call_function(s: &str) -> Option<&str> {
+	if !s.starts_with("call") {
+		return None;
+	}
+	
 	let splitted = s.split_ascii_whitespace();
 	let name = splitted.last().unwrap();
 
 	if name == "call" {
 		panic!("parsing error no fnc name");
 	}
-	return name[1..name.len() - 1].into();
+
+	return Some(&name[1..name.len() - 1]);
 }
 
 // fn assign_callees(mut map: HashMap<String, Caller>) -> HashMap<String, Caller> {
